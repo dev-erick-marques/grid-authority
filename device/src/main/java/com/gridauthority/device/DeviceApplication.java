1cc39e71ae254203bbedd6bab277898ac9f93b26
Erick Marques <erickmarques2564@gmail.com>
2026-03-01 11:52:20 -0300

    chore(device/bootstrap): enable ConfigurationProperties and wire application context Annotate DeviceApplication with @EnableConfigurationProperties to activate simulation config binding

--- device/src/main/java/com/gridauthority/device/DeviceApplication.java
+++ device/src/main/java/com/gridauthority/device/DeviceApplication.java
@@ -1,9 +1,12 @@
 package com.gridauthority.device;
 
+import com.gridauthority.device.infrastructure.config.DeviceSimulationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(DeviceSimulationProperties.class)
 public class DeviceApplication {
 
 	public static void main(String[] args) {
